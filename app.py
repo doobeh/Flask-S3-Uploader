7ea72388d84b1162df3ea2f8d22b1b0b844222e3
Anthony Plunkett <anthony@thefort.org>
2014-11-01 23:59:51 -0400

    Removed unused imports, tweaked Werkzeug import and general PEP8 formatting improvements.

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 from flask import Flask, render_template, flash
 from flask_wtf import Form
 from flask_wtf.file import FileField
-from wtforms import TextField
-from wtforms.validators import DataRequired
 from tools import s3_upload
 
 app = Flask(__name__)
 app.config.from_object('config')
 
+
 class UploadForm(Form):
     example = FileField('Example File')
 
-@app.route('/',methods=['POST','GET'])
+
+@app.route('/', methods=['POST', 'GET'])
 def upload_page():
     form = UploadForm()
     if form.validate_on_submit():
         output = s3_upload(form.example)
         flash('{src} uploaded to S3 as {dst}'.format(src=form.example.data.filename, dst=output))
-    return render_template('example.html',form=form)
+    return render_template('example.html', form=form)
+
 
 if __name__ == '__main__':
     app.run()
--- tools.py
+++ tools.py
@@ -2,10 +2,11 @@ from uuid import uuid4
 import boto
 import os.path
 from flask import current_app as app
-from werkzeug import secure_filename
+from werkzeug.utils import secure_filename
 
-def s3_upload(source_file,acl='public-read'):
-    ''' Uploads WTForm File Object to Amazon S3
+
+def s3_upload(source_file, acl='public-read'):
+    """ Uploads WTForm File Object to Amazon S3
 
         Expects following app.config attributes to be set:
             S3_KEY              :   S3 API Key
@@ -17,7 +18,7 @@ def s3_upload(source_file,acl='public-read'):
         public-read.  It also generates a unique filename via
         the uuid4 function combined with the file extension from
         the source file.
-    '''
+    """
 
     source_filename = secure_filename(source_file.data.filename)
     source_extension = os.path.splitext(source_filename)[1]
@@ -28,7 +29,7 @@ def s3_upload(source_file,acl='public-read'):
     conn = boto.connect_s3(app.config["S3_KEY"], app.config["S3_SECRET"])
     b = conn.get_bucket(app.config["S3_BUCKET"])
 
-    sml = b.new_key("/".join([app.config["S3_UPLOAD_DIRECTORY"],destination_filename]))
+    sml = b.new_key("/".join([app.config["S3_UPLOAD_DIRECTORY"], destination_filename]))
     sml.set_contents_from_string(source_file.data.readlines())
     sml.set_acl(acl)
 
